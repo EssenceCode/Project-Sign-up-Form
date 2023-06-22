72c0980f214be5728cec239c5899947aa93f16c8
EssenceCode <123456789+odin@users.noreply.github.com>
2023-06-22 08:50:24 +0800

    updated errorPwd class

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const form = document.querySelector("form");
 const firstPwd = document.getElementById("pwd");
 const secondPwd = document.getElementById("error-pwd");
-const errorPwd = document.querySelector(".pwd-symbol");
+const errorPwd = document.querySelector(".error");
 
 
 form.addEventListener("submit", (e) => {
@@ -18,7 +18,7 @@ firstPwd.addEventListener("input", (e) => {
 secondPwd.addEventListener("input",showError);
 
 function showError() {
-    if (firstPwd.value !== secondPwd.value) {
+    if (firstPwd.value !== secondPwd.value && secondPwd.value.length >= 8) {
         secondPwd.className = "invalid";
         errorPwd.textContent = "Password mismatch."
     } else {
